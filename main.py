391c1829b96147bc2e926e720aa987745f86012e
Your Name <you@example.com>
2024-05-08 21:49:04 +0300

    the first version

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.widget import get_user_data, mask_account_card
+
+print(mask_account_card("Visa Platinum 8990922113665229"))
+
+print(mask_account_card("Счет 73654108430135874305"))
+
+print(get_user_data("2018-07-11T02:26:18.671407"))
---
+++ src/widget.py
@@ -0,0 +1,18 @@
+from src.masks import get_mask_account, get_mask_card
+
+
+def mask_account_card(number: str) -> str:
+    """Функция принимает строку и маскирует номер карты или счета"""
+    if len(number.split()[-1]) == 16:
+        mask_card = get_mask_card(number.split()[-1])
+        result = f"{number[:-16]}{mask_card}"
+    elif len(number.split()[-1]) == 20:
+        mask_card = get_mask_account(number.split()[-1])
+        result = f"{number[:-20]}{mask_card}"
+    return result
+
+
+def get_user_data(old_data: str) -> str:
+    """Функция принимает строку с датой и форматирует ее"""
+    data_reverse = old_data[0:10].split("-")
+    return ".".join(data_reverse[::-1])
