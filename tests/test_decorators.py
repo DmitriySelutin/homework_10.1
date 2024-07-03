5ad74c198f3c315f7a4422c0eebbce7e4fde9832
Dmitriy <pblpra8@gmail.com>
2024-07-03 18:42:54 +0300

    README.md, main.py, utils.py, test_utils.py

---
+++ tests/test_decorators.py
@@ -0,0 +1,34 @@
+import os
+from typing import Union
+
+import pytest
+from src.decorators import log
+
+
+def test_log_file_raise() -> None:
+    """Тест записи в файл если произошла ошибка"""
+
+    @log(filename="mylog.txt")
+    def example_function(x: int, y: int) -> Union[int, float]:
+        raise ValueError("Что то пошло не так")
+
+    with pytest.raises(ValueError, match="Что то пошло не так"):
+        example_function(3, 10)
+
+    with open(os.path.join(r"logs", "mylog.txt"), "rt") as file:
+        for line in file:
+            massage = line
+
+    assert massage == "example_function ValueError: Что то пошло не так. Inputs: (3, 10), {}\n"
+
+
+def test_log_console(capsys: pytest.CaptureFixture[str]) -> None:
+    @log()
+    def example_function(x, y):
+        return x * y
+
+    result = example_function(5, 100)
+    captured = capsys.readouterr()
+
+    assert captured.out == "example_function ok. Result: 500\n"
+    assert result == 500
